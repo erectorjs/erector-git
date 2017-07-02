5f68ba1690403126c855f2014cb94767a8a2dd1c
Vladimir Kalmykov <v.kalmykov@infotech.team>
2017-07-03 00:03:19 +0300

    #0.1.3: fix unreachable expression

--- helpers/getRemoteUrl.js
+++ helpers/getRemoteUrl.js
@@ -4,7 +4,6 @@ export default function* getGitRemoteUrl({ cwd } = {}) {
   const origin = yield exec('git remote get-url origin', cwd ? {
     cwd,
   } : undefined);
-  const purl = getAddrFromOriginExpr.exec(origin.trim());
   const url = purl.trim();
   if (!url) {
     throw new Error("There is no remote url");
